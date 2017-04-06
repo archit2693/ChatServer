094e027a760a5c76420663db656104abb24a272d
Iron_Man <archit2693.aj@gmail.com>
2017-04-06 16:12:52 +0530

    second  commit

---
+++ app.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+var Pusher = require('pusher');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+
+var pusher = new Pusher({
+  appId: '324569',
+  key: 'c55be1b1d956db3eedd0',
+  secret: 'f3eb63860417bb612f46',
+  cluster: 'ap2',
+  encrypted: true
+});
+
+// pusher.trigger('my-channel', 'my-event', {
+//   "message": "hello world"
+// });
+
+
+app.post('/messages', function(req, res){
+  var message = req.body;
+  res.render('index', { title: 'Message' });
+  pusher.trigger('messages', 'new_message', message);
+  res.json({success: 200});
+});
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
+
+
+module.exports = app;
+
+
